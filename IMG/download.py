56adfa3b9de2de6025c198b707a32c3d0c62d8c0
minhduc1212 <minhduc6e12@gmail.com>
2023-07-21 17:41:42 +0700

    Found and fixed the conflict: cause the blank in chap_name

--- IMG/download.py
+++ IMG/download.py
@@ -20,7 +20,7 @@ response=scraper.get(url, headers=headers)
 soup=BeautifulSoup(response.text, "html.parser") 
     
 comic_name=soup.find('h1', {'class':'title-detail'}).text  
-comic_name = re.sub(r'[\\/:*?"<>|]', ' ', comic_name)
+comic_name = re.sub(r'[\/:*?"<>|]', ' ', comic_name)
 
 sleep(0.5)
 
@@ -30,13 +30,14 @@ if not os.path.exists(path):
 if not os.path.exists(os.path.join(path, comic_name)):
     os.makedirs(os.path.join(path, comic_name))
 
-with open('{}.json'.format(comic_name), 'r') as f:
+with open('{}.json'.format(comic_name), 'r', encoding='utf-8') as f:
     data = json.load(f)
 
 for record in data:
     chap_imgs = record['image_links']
     chap_name = record['chapter_name']
-    chap_name = re.sub(r'[\\/:*?"<>|]', ' ', chap_name)
+    chap_name = re.sub(r'[\/:*?"<>|]', ' ', chap_name)
+    chap_name = " ".join(chap_name.split())
 
     chap_path = os.path.join(os.path.join(path, comic_name), chap_name)
     if not os.path.exists(chap_path):
@@ -54,4 +55,5 @@ for record in data:
         with open(os.path.join(chap_path, filename), 'wb') as f:
             f.write(response_img.content)
         img_count += 1
-        sleep(0.5)
+    print("Đã tải xong ", chap_name)
+    sleep(0.5)
--- IMG/download_test.py
+++ IMG/download_test.py
@@ -1,10 +1,59 @@
+from bs4 import BeautifulSoup
+import cloudscraper
+import os
+from time import sleep
+from fake_useragent import UserAgent
+from urllib.parse import urljoin    
+import re
 import json
 
-with open('Yumin ni Gomeshi o Tabe Sasetai.json', 'r') as f:
+scraper = cloudscraper.create_scraper()
+ua = UserAgent()
+headers =   {
+                'User-Agent': ua.random,
+                'Referer': 'https://www.nettruyenmax.com/'
+            }
+
+url=input("Link Truyện: ", )
+
+response=scraper.get(url, headers=headers)
+soup=BeautifulSoup(response.text, "html.parser") 
+    
+comic_name=soup.find('h1', {'class':'title-detail'}).text  
+comic_name = re.sub(r'[\/:*?"<>|]', ' ', comic_name)
+
+sleep(0.5)
+
+path = input("Vị Trí Tải Truyện: ",)
+if not os.path.exists(path):
+    os.makedirs(path)
+if not os.path.exists(os.path.join(path, comic_name)):
+    os.makedirs(os.path.join(path, comic_name))
+
+with open('{}.json'.format(comic_name), 'r', encoding='utf-8') as f:
     data = json.load(f)
 
 for record in data:
-    name = record['chapter_name']
     chap_imgs = record['image_links']
-    chap_imgs_str = ', '.join(chap_imgs)
-    print(chap_imgs_str)
+    chap_name = record['chapter_name']
+    chap_name = re.sub(r'[\/:*?"<>|]', ' ', chap_name)
+    chap_name = " ".join(chap_name.split())
+
+    chap_path = os.path.join(os.path.join(path, comic_name), chap_name)
+    if not os.path.exists(chap_path):
+        os.makedirs(chap_path)
+
+    img_count=1
+    for img_link in chap_imgs:
+                
+        img_link_fix=urljoin(url, img_link)
+
+        response_img=scraper.get(img_link_fix, headers=headers)
+
+        filename =f'{img_count:03}.jpg'
+   
+        with open(os.path.join(chap_path, filename), 'wb') as f:
+            f.write(response_img.content)
+        img_count += 1
+    print("Đã tải xong ", chap_name)
+    sleep(0.5)
